0f82c5556e0e6658e406ab93738f633d9d1a26df
Chandu2413 <chandanschadans227@gmail.com>
2025-10-17 09:34:52 +0530

    fixed jpg error

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-//import Profile from "../../src/assets/profile.jpg" // Adjust the path if needed
 import "./Home.css";
 
 function Home() {
   return (
     <section id="home" className="home">
       <div className="home-content">
-        {/* <img src={Profile} alt="Chandan S" className="profile-img" /> */}
+        <img src="/profile.jpg" alt="Chandan S" className="profile-img" />
         <div className="intro-text">
           <h1>Hello, I'm <span>Chandan S</span></h1>
           <p>Frontend Developer | React Enthusiast | Web Designer</p>
